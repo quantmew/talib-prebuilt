8b78e8f5e962ddd1dc8e4f7c344024273279d31b
jstzwj <1103870790@qq.com>
2025-01-05 03:14:07 +0800

    install polars before test

--- setup_rust.sh
+++ setup_rust.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain nightly -y
 echo 'source $HOME/.cargo/env' >> $HOME/.bashrc
-export PATH="${HOME}/.cargo/bin:${PATH}"
+. "$HOME/.cargo/env"
