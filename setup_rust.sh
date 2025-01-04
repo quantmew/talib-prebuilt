13b6c538d6e763aa45ed0c950914a7ad7f04d233
jstzwj <1103870790@qq.com>
2025-01-05 02:58:18 +0800

    setup rust bug fix

--- setup_rust.sh
+++ setup_rust.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain nightly -y
 echo 'source $HOME/.cargo/env' >> $HOME/.bashrc
-export PATH="/root/.cargo/bin:${PATH}"
+export PATH="${HOME}/.cargo/bin:${PATH}"
